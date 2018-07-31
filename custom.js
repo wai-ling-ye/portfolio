5eee05ada6fda2fc0028b6202056bd0fa5d0b509
Wai Ling Ye <wai.ling.ye@gmail.com>
2018-07-31 13:20:56 -0400

    delete image to refresh file name

--- custom.js
+++ custom.js
@@ -1,25 +1,81 @@
 $(document).ready(function(){
   // Smooth Scrolling; source: https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  void function() {
+  'use strict';
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+  var ANCHOR_SELECTOR = 'a[href*="#"]:not([href="#"]):not([href="#0"])';
+  var CHECK_INTERVAL = 50;
 
-      // Store hash
-      var hash = this.hash;
+  var currentTimeout;
+  var currentScrollY;
+  var anchors = document.querySelectorAll(ANCHOR_SELECTOR);
+  var anchor;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
+  for (let i = 0; i < anchors.length; i++) {
+    anchor = anchors[i];
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+    ensurePathnameLacksTerminalSlash(anchor);
+
+    if (!pathnameIsLocal(anchor)) continue;
+
+    anchor.addEventListener('click', scrollToAnchor);
+  }
+
+  function checkFocus() {
+    if (currentScrollY === window.pageYOffset) {
+      this.focus();
+
+      try {
+        if (this.matches(':focus')) return;
+      } catch (err) {
+        // IE11
+      }
+
+      this.tabIndex = -1;
+      this.focus();
+
+      return;
+    }
+
+    currentScrollY = window.pageYOffset;
+    currentTimeout = setTimeout(checkFocus.bind(this), CHECK_INTERVAL);
+  }
+
+  function ensurePathnameLacksTerminalSlash(anchor) {
+    if (anchor.pathname[anchor.pathname.length - 1] === '/') {
+      anchor.pathname = anchor.pathname.slice(0, -1);
+    }
+  }
+
+  function pathnameIsLocal(anchor) {
+    return (
+      anchor.hostname === location.hostname &&
+      anchor.pathname === location.pathname
+    );
+  }
+
+  function scrollToAnchor(event) {
+    var id = this.hash.slice(1);
+
+    var target =
+      document.getElementById(id) ||
+      document.getElementsByName(id)[0];
+
+    if (!target) return;
+
+    currentScrollY = window.pageYOffset;
+
+    clearTimeout(currentTimeout);
+
+    event.preventDefault();
+
+    target.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+
+    currentTimeout = setTimeout(checkFocus.bind(target), CHECK_INTERVAL);
+  }
+};
 });
